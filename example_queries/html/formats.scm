5aa4981b6b12c84323604679a0823af25f895753
Pham Huy Hoang <hoangtun0810@gmail.com>
2023-12-04 23:56:40 +0900

    update example_queries
    Don't use inherit to lessen the learning overhead

--- example_queries/html/formats.scm
+++ example_queries/html/formats.scm
@@ -1,4 +1,57 @@
-; inherits: html_tags
+(element
+  (start_tag
+    (tag_name) @_foo
+    (#not-eq? @_foo "html")) @format.indent.begin
+  (_)
+  (end_tag
+    ">") @format.indent.dedent @format.indent.end)
+
+(element
+  (start_tag
+    (tag_name) @_foo) @format.append-newline
+  (#eq? @_foo "html"))
+
+(element
+  [
+    (self_closing_tag
+      (tag_name) @_foo)
+    (start_tag
+      (tag_name) @_foo)
+  ]
+  (#not-any-of? @_foo "br" "i" "em" "b" "u" "wbr")) @format.append-newline
+
+(self_closing_tag
+  (tag_name)
+  (_) @format.prepend-space)
+(self_closing_tag
+  (tag_name) @format.indent.begin
+  "/>" @format.indent.end
+  (#set! format.conditional))
+
+(start_tag
+  (tag_name)
+  (_) @format.prepend-space)
+(start_tag
+  (tag_name) @format.indent.begin
+  ">" @format.indent.end
+  (#set! format.conditional))
+
+(comment) @format.append-newline
+
+(script_element
+  (start_tag) @format.append-newline
+  (end_tag) @format.prepend-newline) @format.append-newline
+
+(style_element
+  (start_tag) @format.append-newline
+  (end_tag) @format.append-newline) @format.append-newline
+
+(element
+  (start_tag
+    (tag_name) @_foo)
+  (text) @format.ignore
+  (end_tag) @format.prepend-newline
+  (#eq? @_foo "code")) 
 
 (doctype) @format.append-newline
 (text) @format.append-newline
